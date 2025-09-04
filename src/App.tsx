aec9b441022a2642e8b02342b8d5821d7232fabb
Vivian Tang <viviantang.dev@gmail.com>
2025-09-04 15:12:40 +0200

    Created Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 function App() {
-  return <>hello</>;
+  return (
+    <Routes>
+      <Route path='/' element={<h1>home</h1>} />
+      <Route path='/new' element={<h1>new</h1>} />
+      <Route path='/:id'>
+        <Route index element={<h1>Show</h1>} />
+        <Route path='edit' element={<h1>Edit</h1>} />
+      </Route>
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  );
 }
 
 export default App;
